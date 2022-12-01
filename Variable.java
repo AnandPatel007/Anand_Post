529bea3560a30cc5ece2e3b3f69b88a99581d2a8
AnandPatel007 <apatel5541@students.mc3.edu>
2022-11-30 21:00:28 -0500

    Create Variable.java
    The value is 5.

---
+++ Variable.java
@@ -0,0 +1,12 @@
+
+public class Variable
+{
+   public static void main(String [] args)
+   {
+      int value;
+      
+      value = 5;
+      System.out.println("The value is ");
+      System.out.println(value);
+    }
+   } 
